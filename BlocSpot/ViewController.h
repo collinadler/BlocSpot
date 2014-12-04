5827da417292bba9694abb243515f462015a42f4
Collin Adler <collinadler@gmail.com>
2014-12-04 13:10:28 -0600

    Initial Commit

---
+++ BlocSpot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocSpot
+//
+//  Created by Collin Adler on 12/4/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
